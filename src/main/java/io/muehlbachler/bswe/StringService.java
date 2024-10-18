fb023888b88a5ce4702cfceed56e3f889bac7993
souri <souri@wp-entwickler.at>
2024-10-18 20:51:46 +0200

    touppercase Methode implemenieren die Zeichen in Großbuchstaben umwandeln
    Todo für Edgecase großes scharfes S

--- src/main/java/io/muehlbachler/bswe/StringService.java
+++ src/main/java/io/muehlbachler/bswe/StringService.java
@@ -2,12 +2,23 @@ package io.muehlbachler.bswe;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Locale;
+
 // FIXME: add javadoc
 @Service
 public class StringService {
-    // FIXME: add javadoc
+
+    /**
+     * Nimmt String entgegen und gibt ihn in UPPERCASE Form, also GROẞGESCHRIEBEN zurück
+     * todo großes scharfes S (=ẞ) implementieren
+     * https://de.wikipedia.org/wiki/Gro%C3%9Fes_%C3%9F
+     * @param string
+     * @return
+     */
     public String toUpperCase(final String string) {
-        // FIXME: implement
-        return string;
+        if (  null == string ){
+            return null;
+        }
+        return string.toUpperCase(Locale.getDefault());
     }
 }
