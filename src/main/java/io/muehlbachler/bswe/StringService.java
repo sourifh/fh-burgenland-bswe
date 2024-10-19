1bf0fa904bdc6c05133a42dc114da46904d9b7f8
souri <souri@wp-entwickler.at>
2024-10-19 11:53:20 +0200

    Doku erweitert

--- src/main/java/io/muehlbachler/bswe/StringService.java
+++ src/main/java/io/muehlbachler/bswe/StringService.java
@@ -12,8 +12,8 @@ public class StringService {
      * Nimmt String entgegen und gibt ihn in UPPERCASE Form, also GROẞGESCHRIEBEN zurück
      * todo großes scharfes S (=ẞ) implementieren
      * <a href="https://de.wikipedia.org/wiki/Gro%C3%9Fes_%C3%9F">...</a>
-     * @param string    Zeichen die in Großbuchstaben umgewandelt werden sollen
-     * @return string   Übergebene Zeichen in Großbuchstaben oder null on failure
+     * @param  string    Zeichen die in Großbuchstaben umgewandelt werden sollen
+     * @return string    Übergebene Zeichen in Großbuchstaben oder null on failure
      */
     public String toUpperCase(final String string) {
         if (  null == string ){
